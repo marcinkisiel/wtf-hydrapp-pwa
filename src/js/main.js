2ba32a9a153f445f6d3b0967d1f9d8af4a5065b6
Marcin Kisiel <kisiel.marcin@gmail.com>
2019-12-18 23:58:20 +0100

    feat: add basic JS logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,43 @@
-"use strict";
+'use strict';
 
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('serviceworker.js').then(
+			function(registration) {
+				// Registration was successful
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			},
+			function(err) {
+				// registration failed :(
+				console.log('ServiceWorker registration failed: ', err);
+			}
+		);
+	});
 }
 
 // place your code below
 
+const buttonAdd = document.querySelector('.button_add-glass--js');
+const buttonSubtract = document.querySelector('.button_subtract-glass--js');
+const glassCounter = document.querySelector('.glass__counter--js');
+let actualGlassNumber = glassCounter.innerHTML;
+console.log(actualGlassNumber);
 
-console.log(`Hello world!`);
+buttonAdd.addEventListener('click', addGlass);
+buttonSubtract.addEventListener('click', subtractGlass);
 
+function addGlass() {
+	if (actualGlassNumber < 9) {
+		actualGlassNumber++;
+		glassCounter.innerHTML = actualGlassNumber;
+	}
+}
 
+function subtractGlass() {
+	if (actualGlassNumber > 0) {
+		actualGlassNumber--;
+		glassCounter.innerHTML = actualGlassNumber;
+	}
+}
