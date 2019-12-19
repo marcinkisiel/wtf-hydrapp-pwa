f40b7bcc5b0e50cd713a86f63b684fb26d5531f5
Marcin Kisiel <kisiel.marcin@gmail.com>
2019-12-19 19:41:31 +0100

    add switch to water animation

--- src/js/main.js
+++ src/js/main.js
@@ -31,7 +31,7 @@ const keyDate = new Date().toISOString().slice(0, 10);
 const checkDate = localStorage.getItem(keyDate);
 
 let actualGlassNumber;
-let glassBlueHeight;
+let glassBlueHeight = 0;
 
 if (checkDate == null) {
 	actualGlassNumber = 0;
@@ -39,6 +39,51 @@ if (checkDate == null) {
 	actualGlassNumber = localStorage.getItem(keyDate);
 }
 glassCounter.innerHTML = actualGlassNumber;
+
+function setGlassBlueHeight(height) {
+	glassBlueHeight = height;
+	console.log(height);
+	glassBlueHeight = glassBlueHeight.toString();
+	glassBasicBlue.setAttribute('height', glassBlueHeight);
+}
+
+console.log('actualGlassNumber before switch: ' + actualGlassNumber + typeof actualGlassNumber);
+console.log('glassBlueHeight before switch: ' + glassBlueHeight);
+
+switch (parseInt(actualGlassNumber)) {
+	case 1:
+		setGlassBlueHeight(41);
+		break;
+	case 2:
+		setGlassBlueHeight(81);
+		break;
+	case 3:
+		setGlassBlueHeight(121);
+		break;
+	case 4:
+		setGlassBlueHeight(161);
+		break;
+	case 5:
+		setGlassBlueHeight(201);
+		break;
+	case 6:
+		setGlassBlueHeight(241);
+		break;
+	case 7:
+		setGlassBlueHeight(281);
+		break;
+	case 8:
+		setGlassBlueHeight(321);
+		break;
+	case 9:
+		setGlassBlueHeight(361);
+		break;
+	case 10:
+		setGlassBlueHeight(401);
+		break;
+}
+console.log('actualGlassNumber after switch: ' + actualGlassNumber);
+console.log('glassBlueHeight after switch: ' + glassBlueHeight);
 
 buttonAdd.addEventListener('click', addGlass);
 buttonSubtract.addEventListener('click', subtractGlass);
