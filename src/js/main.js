2e3df5861c809e3904daffe3fb18ec99fee2d04e
Marcin Kisiel <kisiel.marcin@gmail.com>
2019-12-20 11:24:20 +0100

    feat: remove switch from water animation

--- src/js/main.js
+++ src/js/main.js
@@ -40,50 +40,14 @@ if (checkDate == null) {
 }
 glassCounter.innerHTML = actualGlassNumber;
 
-function setGlassBlueHeight(height) {
-	glassBlueHeight = height;
-	console.log(height);
+function setGlassBlueHeight(glasses) {
+	glasses = parseInt(glasses);
+	glassBlueHeight = glasses * 40 + 1;
 	glassBlueHeight = glassBlueHeight.toString();
 	glassBasicBlue.setAttribute('height', glassBlueHeight);
 }
 
-console.log('actualGlassNumber before switch: ' + actualGlassNumber + typeof actualGlassNumber);
-console.log('glassBlueHeight before switch: ' + glassBlueHeight);
-
-switch (parseInt(actualGlassNumber)) {
-	case 1:
-		setGlassBlueHeight(41);
-		break;
-	case 2:
-		setGlassBlueHeight(81);
-		break;
-	case 3:
-		setGlassBlueHeight(121);
-		break;
-	case 4:
-		setGlassBlueHeight(161);
-		break;
-	case 5:
-		setGlassBlueHeight(201);
-		break;
-	case 6:
-		setGlassBlueHeight(241);
-		break;
-	case 7:
-		setGlassBlueHeight(281);
-		break;
-	case 8:
-		setGlassBlueHeight(321);
-		break;
-	case 9:
-		setGlassBlueHeight(361);
-		break;
-	case 10:
-		setGlassBlueHeight(401);
-		break;
-}
-console.log('actualGlassNumber after switch: ' + actualGlassNumber);
-console.log('glassBlueHeight after switch: ' + glassBlueHeight);
+setGlassBlueHeight(actualGlassNumber);
 
 buttonAdd.addEventListener('click', addGlass);
 buttonSubtract.addEventListener('click', subtractGlass);
